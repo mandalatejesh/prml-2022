39e4a2c5307a0d8066b4229721b1dd41844ccead
mandalatejesh <mandalatejesh01@gmail.com>
2022-01-26 16:21:35 +0530

    modified

--- A1/test.py
+++ A1/test.py
@@ -1,21 +1,30 @@
-import matplotlib.image as img
-import matplotlib.pyplot as plt
+# import matplotlib.image as img
+# import matplotlib.pyplot as plt
+from PIL import Image
 from numpy import linalg as numLib
 from sklearn.decomposition import PCA, IncrementalPCA
 import imageio
 import numpy as np
 
+
 def recoverData(Z, U, K):
-    # Compute the approximation of the data by projecting back onto 
-    # the original space using the top K eigenvectors in U. 
+    # Compute the approximation of the data by projecting back onto
+    # the original space using the top K eigenvectors in U.
     # Z: projected data
 
     new_U = U[:, :K]
+    # We can use transpose instead of inverse because U is orthogonal.
     return Z.dot(new_U.T)
 
-image = img.imread('.\problem\\69.jpg')
-eigen_values, eigen_vectors = numLib.eig(image)
-for k in [10, 50, 100, 150, 200, 250]:
-    ipca = IncrementalPCA(n_components=k)
-    img_recon = ipca.inverse_transform(ipca.fit_transform(image))
-    imageio.imwrite(".\output\pca_output_"+str(k)+".jpg", img_recon)
+
+image = Image.open(r'./problem/69.jpg')
+array = np.array(image)
+print(array[0])
+eigen_values, eigen_vectors = numLib.eig(array*(array.T))
+# for k in [10, 50, 100, 150, 200, 250]:
+#     ipca = IncrementalPCA(n_components=k)
+#     img_recon = ipca.inverse_transform(ipca.fit_transform(image))
+#     imageio.imwrite("./output/pca_output_"+str(k)+".jpg", img_recon)
+np.abs(eigen_vectors)
+new = recoverData(array, eigen_vectors, 10)
+imageio.imwrite("./output/normal.jpg", new)
